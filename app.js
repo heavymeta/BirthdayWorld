81f2810350ee8c79736bf6e59d9de8b66552c7aa
heavymeta <ian@heavy-meta.com>
2016-04-18 17:14:45 -0400

    db

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 var app = express(); //module.exp = express.createServer();
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars.engine);
 
 // Configuration
 
