94c79a752100f1fe2cd11663642464d32151853b
heavymeta <ian@heavy-meta.com>
2016-04-18 15:00:04 -0400

    db

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require('express')
 , serveStatic = require('serve-static')
 , errorHandler = require('errorhandler');
 
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/mongo_test");
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://birthdayworld_admin:9fpG7Ln9ez93@ds011271.mlab.com:11271/heroku_1ptkn26d");
 
 var app = express(); //module.exp = express.createServer();
 
